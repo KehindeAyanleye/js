cf42ea10f97bb6f1ed21b28b2fc14941bb8d918d
Kehinde Ayanleye <Kehindeayanleye@gmail.com>
2014-09-26 16:19:15 +0100

    Update accessorProperty.js

--- accessorProperty.js
+++ accessorProperty.js
@@ -1,5 +1,7 @@
 //Accessor Property
 //accessor property uses get and or set
+//here we are defining a property for an object called inch
+//first we try get myObject.inch and the return would be a computation of this.mm divided by 25.4
 Object.defineProperty(my_object,
 	'inch', {
 		get: funtion () {
