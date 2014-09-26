e5ea21436d6d4629615cd5a5ff748a1aa175e1cc
Kehinde Ayanleye <Kehindeayanleye@gmail.com>
2014-09-26 12:43:28 +0100

    Create yahooTrivia.js

---
+++ yahooTrivia.js
@@ -0,0 +1,14 @@
+
+//Trivia Object Yahoo
+YAHOO.Trivia = function() {
+	// define your commom vars here
+	// define your commom functions here
+	return {
+		getNextPoser: function(cat, diff) {
+			...
+		},
+		showPoser: function () {
+			...
+		}
+	};
+} ();
